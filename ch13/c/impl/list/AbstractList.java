b5bb5d398c01ece52bb7622c892622295d9ba87e
updown-90 <52308702+updown-90@users.noreply.github.com>
2021-07-16 20:38:12 +0900

    add: 13장 직접해 봅시다, 정리해 봅시다 작성

---
+++ ch13/c/impl/list/AbstractList.java
@@ -0,0 +1,22 @@
+package c.impl.list;
+
+public class AbstractList implements List{
+	@Override
+	public void add() {
+
+	}
+
+	@Override
+	public void update(int index, Object value) {
+
+	}
+
+	@Override
+	public void remove(int index) {
+
+	}
+
+	public void clear() {
+
+	}
+}
---
+++ ch13/c/impl/list/List.java
@@ -0,0 +1,10 @@
+package c.impl.list;
+
+public interface List {
+
+	public void add();
+
+	public void update(int index, Object value);
+
+	public void remove(int index);
+}
